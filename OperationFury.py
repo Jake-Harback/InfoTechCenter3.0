94bae3b319ba319cdd5187a068b0143e280dcf85
Jake Harback <01jharback@gmail.com>
2022-10-05 08:21:45 -0400

    Final Touches On Welcome Screen

--- OperationFury.py
+++ OperationFury.py
@@ -10,16 +10,17 @@ Our Welcome Screen will start our program letting drivers know that the InfoTech
 from time import sleep #We imported the sleep function from the time library
 
 print("\n\n\033[1;36m Welcome to Operation Fury InfoTech Center")
-sleep(2)
+sleep(1.5)
 print("\n\033[1;36m Operation Fury's Operating System is Booting Up\n\033[0m")
+sleep(1.5)
 
 #Code to print a different color
 #print("\033[1;36m This text is Bright Green\n\033[0m")
 
 
-for i in range(2):
+for i in range(8):
     print("OS Booting Up")
-    sleep(.5)
+    sleep(1)
 
 
 
