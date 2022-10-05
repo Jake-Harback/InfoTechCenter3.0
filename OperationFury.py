1c76a2f74637bc2fbfe3d71613a66032f7923f9d
Jake Harback <01jharback@gmail.com>
2022-10-05 08:39:09 -0400

    Initial Weather Commit

---
+++ OperationFury.py
@@ -0,0 +1,8 @@
+#Weather
+#Developer: Jake Harback
+#Version: 1.0
+
+"""
+Create a function for our current weather using the random.choice function to determine what the weather is picking
+from a list - using If, Elif & Else statements to check the condition and print a print line
+"""
